2a0b69cdaa4f3a1ed845ffb293a4fb36546e6e0e
OIZHENHAU <zhenhau8072@gmail.com>
2025-05-24 02:16:51 +0800

    Adding Recent Lesson

--- ps2.py
+++ ps2.py
@@ -9,6 +9,15 @@ from typing import List, Tuple
 
 import cube
 import utils
+import copy
+import json
+import cube
+import utils
+from ast import literal_eval
+from utils import PriorityQueue
+from utils import Node
+from typing import Dict, Iterable, List, Optional, Tuple, Union
+Action = List[Union[int, str]]
 
 """ ADD HELPER FUNCTION HERE """
 
@@ -22,23 +31,15 @@ from utils import PriorityQueue
 #TODO Task 1.1: Implement your heuristic function, which takes in an instance of the Cube and
 #   the State class and returns the estimated cost of reaching the goal state from this state.
 def heuristic_func(problem: cube.Cube, state: cube.State) -> float:
-    r"""
-    Computes the heuristic value of a state
-    
-    Args:
-        problem (cube.Cube): the problem to compute
-        state (cube.State): the state to be evaluated
-
-    Returns:
-        h_n (float): the heuristic value 
-    """
-    h_n = 0.0
+    h_n = 0
     goals = problem.goal
-
-    """ YOUR CODE HERE """
-
-    """ END YOUR CODE HERE """
-
+    initial_layout = state.layout
+    goal_layout = goals.layout
+    matrix = goals.shape
+    for i in range(matrix[0] * matrix[1]):
+        if (initial_layout[i] != goal_layout[i]):
+            h_n += 1
+    h_n = h_n / 2
     return h_n
 
 # Test
@@ -100,7 +101,7 @@ def astar_search(problem: cube.Cube):
     r"""
     A* Search finds the solution to reach the goal from the initial.
     If no solution is found, return False.
-    
+
     Args:
         problem (cube.Cube): Cube instance
 
@@ -109,11 +110,41 @@ def astar_search(problem: cube.Cube):
     """
     fail = True
     solution = []
-    
-    """ YOUR CODE HERE """
- 
-    """ END YOUR CODE HERE """
-    
+    queue = utils.PriorityQueue()
+    initial_problem = problem.initial
+    goals = problem.goal
+    initial_state = cube.State(initial_problem.shape, initial_problem.layout)
+    # print(initial_state)
+    h_n = heuristic_func(problem, initial_problem)
+    # print(h_n)
+    g_n = 0
+    initial_node = utils.Node(None, solution, initial_state, g_n, h_n)
+    # print(initial_node)
+    queue.push(g_n + h_n, initial_node)
+    # print(queue)
+    while queue:
+        curr_node = queue.pop()
+        # print(curr_node.state)
+        if (problem.goal_test(curr_node.state)):
+            fail = False
+            solution = curr_node.act
+            return solution
+        else:
+            new_actions = problem.actions(curr_node.act)
+            # print(new_actions)
+            for action in new_actions:
+                # print(action)
+                new_state = problem.result(curr_node.state, action)
+                # print(new_state)
+                new_g_n = problem.path_cost(curr_node.g_n, curr_node.state, action, new_state)
+                # print(new_g_n)
+                new_h_n = heuristic_func(problem, new_state)
+                # print(new_h_n)
+                new_act = curr_node.act + [action]
+                new_node = utils.Node(curr_node, new_act, new_state, new_g_n, new_h_n)
+                # print(new_node)
+                new_f_n = new_g_n + new_h_n
+                queue.push(new_f_n, new_node)
     if fail:
         return False
     return solution
@@ -182,22 +213,18 @@ if __name__ == '__main__':
 
 #TODO Task 2.3: Define a reasonable transition function to generate new candidate solutions.
 def transition(route: List[int]):
-    r"""
-    Generates new routes to be used in the next iteration in the hill-climbing algorithm.
-
-    Args:        
-        route (List[int]): The current route as a list of cities in the order of travel
-
-    Returns:
-        new_routes (List[List[int]]): New routes to be considered
-    """
-    new_routes = []
-    
-    """ YOUR CODE HERE """
-    
-    """ END YOUR CODE HERE """
-
-    return new_routes
+    route_result = []
+    rr = route
+    # print(rr)
+    for i in range(len(route)):
+        curr_route = rr.copy()
+        i1, i2 = random.sample(range(len(route)), 2)
+        const = curr_route[i1]
+        curr_route[i1] = curr_route[i2]
+        curr_route[i2] = const
+        route_result.append(curr_route)
+    # print(route_result)
+    return route_result
 
 # Test
 @wrap_test
@@ -215,28 +242,32 @@ if __name__ == '__main__':
 
 
 #TODO Task 2.4: Implement a heuristic to decide on the "goodness" of a route.
-def heuristic(cities: int, distances: List[Tuple[int]], route: List[int]):
-    r"""
-    Computes the heuristic value of a route
+from typing import List, Tuple
+import random
 
-    Args:
-        cities (int): The number of cities to be visited
 
-        distances (List[Tuple[int]]): The list of distances between every two cities
-            Each distance is represented as a tuple in the form of (c1, c2, d), where
-                c1 and c2 are the two cities and d is the distance between them.
-            The length of the list should be equal to cities * (cities - 1)/2.
+def heuristic(cities: int, distances: List[Tuple[int]], route: List[int]) -> int:
+    def adjMatrix(cities, distances):
+        matrix = []
+        for i in range(cities):
+            # print(i)
+            matrix.append([])
+            for j in range(cities):
+                matrix[i].append(0)
+        for (a, b, c) in distances:
+            # print(a, b, c)
+            matrix[a][b] = c
+            matrix[b][a] = c
+        return matrix
 
-        route (List[int]): The current route as a list of cities in the order of travel
-
-    Returns:
-        h_n: the heuristic value
-    """
+    adj_matrix = adjMatrix(cities, distances)
     h_n = 0
-    
-    """ YOUR CODE HERE """
-    
-    """ END YOUR CODE HERE """
+    route.append(route[0])
+    length_route = len(route)
+    for i in range(length_route - 1):
+        x = route[i]
+        y = route[i + 1]
+        h_n += (30 - adj_matrix[x][y])
 
     return h_n
 
@@ -259,30 +290,84 @@ if __name__ == '__main__':
 
 #TODO Task 2.6: Implement hill-climbing which takes in the number of cities and the list of
 #   distances and returns the shortest route as a list of cities.
+from typing import List, Tuple
+import random
+
+
+def transition(route: List[int]):
+    route_result = []
+    rr = route
+    # print(rr)
+    for i in range(len(route)):
+        curr_route = rr.copy()
+        i1, i2 = random.sample(range(len(route)), 2)
+        const = curr_route[i1]
+        curr_route[i1] = curr_route[i2]
+        curr_route[i2] = const
+        route_result.append(curr_route)
+    # print(route_result)
+    return route_result
+
+
+def heuristic(cities: int, distances: List[Tuple[int]], route: List[int]) -> int:
+    def adjMatrix(cities, distances):
+        matrix = []
+        for i in range(cities):
+            # print(i)
+            matrix.append([])
+            for j in range(cities):
+                matrix[i].append(0)
+        for (a, b, c) in distances:
+            # print(a, b, c)
+            matrix[a][b] = c
+            matrix[b][a] = c
+        return matrix
+
+    adj_matrix = adjMatrix(cities, distances)
+    h_n = 0
+    route.append(route[0])
+    length_route = len(route)
+    for i in range(length_route - 1):
+        x = route[i]
+        y = route[i + 1]
+        h_n += (30 - adj_matrix[x][y])
+
+    return h_n
+
+
 def hill_climbing(cities: int, distances: List[Tuple[int]]):
-    r"""
-    Hill climbing finds the solution to reach the goal from the initial.
-    
-    Args:
-        cities (int): The number of cities to be visited
-        
-        distances (List[Tuple[int]]): The list of distances between every two cities
-            Each distance is represented as a tuple in the form of (c1, c2, d), where
-                c1 and c2 are the two cities and d is the distance between them.
-            The length of the list should be equal to cities * (cities - 1)/2.
-
-    Returns:
-        route (List[int]): The shortest route, represented by a list of cities
-            in the order to be traversed.
-    """
-
-    route = []
-    
-    """ YOUR CODE HERE """
-
-    """ END YOUR CODE HERE """
-    
-    return route
+    solution = []
+    curr_route = []
+    # print(route)
+    route_cost = 999
+    random_route = transition(list(range(cities)))
+    # print(random_route)
+    for i in range(cities - 1):
+        curr_route_1 = random_route[i]
+        # print(curr_route_1)
+        curr_route_2 = random_route[i + 1]
+        # print(curr_route_2)
+        curr_cost_1 = heuristic(cities, distances, curr_route_1)
+        # print(curr_cost_1)
+        curr_cost_2 = heuristic(cities, distances, curr_route_2)
+        # print(curr_cost_2)
+        if curr_cost_1 <= curr_cost_2:
+            if route_cost > curr_cost_1:
+                curr_route = curr_route_1
+                # print(curr_route)
+                route_cost = curr_cost_1
+                # print(route_cost)
+        else:
+            if route_cost > curr_cost_2:
+                curr_route = curr_route_2
+                # print(curr_route)
+                route_cost = curr_cost_2
+                # print(route_cost)
+    # print(curr_route)
+    for i in range(cities):
+        solution.append(curr_route[i])
+    # print(solution)
+    return solution
 
 # Test
 @wrap_test
@@ -317,32 +402,99 @@ if __name__ == '__main__':
 
 #TODO Task 2.7: Implement hill_climbing_with_random_restarts by repeating hill climbing
 #   at different random locations.
+from typing import List, Tuple
+import random
+
+
+def transition(route: List[int]):
+    route_result = []
+    rr = route
+    # print(rr)
+    for i in range(len(route)):
+        curr_route = rr.copy()
+        i1, i2 = random.sample(range(len(route)), 2)
+        const = curr_route[i1]
+        curr_route[i1] = curr_route[i2]
+        curr_route[i2] = const
+        route_result.append(curr_route)
+    # print(route_result)
+    return route_result
+
+
+def heuristic(cities: int, distances: List[Tuple[int]], route: List[int]) -> int:
+    def adjMatrix(cities, distances):
+        matrix = []
+        for i in range(cities):
+            # print(i)
+            matrix.append([])
+            for j in range(cities):
+                matrix[i].append(0)
+        for (a, b, c) in distances:
+            # print(a, b, c)
+            matrix[a][b] = c
+            matrix[b][a] = c
+        return matrix
+
+    adj_matrix = adjMatrix(cities, distances)
+    h_n = 0
+    route.append(route[0])
+    length_route = len(route)
+    for i in range(length_route - 1):
+        x = route[i]
+        y = route[i + 1]
+        h_n += (30 - adj_matrix[x][y])
+
+    return h_n
+
+
+def hill_climbing(cities: int, distances: List[Tuple[int]]):
+    solution = []
+    curr_route = []
+    # print(route)
+    route_cost = 999
+    random_route = transition(list(range(cities)))
+    # print(random_route)
+    for i in range(cities - 1):
+        curr_route_1 = random_route[i]
+        # print(curr_route_1)
+        curr_route_2 = random_route[i + 1]
+        # print(curr_route_2)
+        curr_cost_1 = heuristic(cities, distances, curr_route_1)
+        # print(curr_cost_1)
+        curr_cost_2 = heuristic(cities, distances, curr_route_2)
+        # print(curr_cost_2)
+        if curr_cost_1 <= curr_cost_2:
+            if route_cost > curr_cost_1:
+                curr_route = curr_route_1
+                # print(curr_route)
+                route_cost = curr_cost_1
+                # print(route_cost)
+        else:
+            if route_cost > curr_cost_2:
+                curr_route = curr_route_2
+                # print(curr_route)
+                route_cost = curr_cost_2
+                # print(route_cost)
+    # print(curr_route)
+    for i in range(cities):
+        solution.append(curr_route[i])
+    # print(solution)
+    return solution
+
+
 def hill_climbing_with_random_restarts(cities: int, distances: List[Tuple[int]], repeats: int = 10):
-    r"""
-    Hill climbing with random restarts finds the solution to reach the goal from the initial.
-    
-    Args:
-        cities (int): The number of cities to be visited
-        
-        distances (List[Tuple[int]]): The list of distances between every two cities
-            Each distance is represented as a tuple in the form of (c1, c2, d), where
-                c1 and c2 are the two cities and d is the distance between them.
-            The length of the list should be equal to cities * (cities - 1)/2.
-
-        repeats (int): The number of times hill climbing to be repeated. The default
-            value is 10.
-
-    Returns:
-        route (List[int]): The shortest route, represented by a list of cities
-            in the order to be traversed.
-    """
-
     route = []
-    
-    """ YOUR CODE HERE """
-
-    """ END YOUR CODE HERE """
-    
+    best_route = []
+    best_cost = 999
+    for i in range(repeats):
+        curr_route = hill_climbing(cities, distances)
+        curr_h_n = heuristic(cities, distances, curr_route)
+        if (curr_h_n < best_cost):
+            best_route = curr_route
+            best_cost = curr_h_n
+    for i in range(cities):
+        route.append(best_route[i])
+    # print(route)
     return route
 
 # Test
